ccfae86ea387a88391d446a8970907ebc4fd062a
BetaKrankri <edominguezj11@outlook.com>
2023-06-19 13:56:11 -0600

    chores: add Tailwind/form

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+    }),
+  ],
 };
