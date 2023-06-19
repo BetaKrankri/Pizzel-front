d3e261ae7797d1924bd4db00f830a1a0f4898962
BetaKrankri <edominguezj11@outlook.com>
2023-06-19 13:57:05 -0600

    component: Input

--- src/components/form-components/Input.tsx
+++ src/components/form-components/Input.tsx
@@ -1,12 +1,34 @@
-const Input = () => {
+import React, { InputHTMLAttributes } from "react";
+
+interface Props {
+  type: "text" | "password" | "email";
+  label: string;
+  placeholder?: string;
+  labelStyle?: string;
+  inputStyle?: string;
+  value?: string | number | readonly string[] | undefined;
+  onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
+}
+
+const Input = ({
+  type,
+  labelStyle,
+  inputStyle,
+  label,
+  placeholder,
+  value,
+  onChange,
+}: Props) => {
   return (
     <div>
-      <label className={` flex flex-col gap-2`}>
-        <p>Label</p>
+      <label className={` flex flex-col gap-2 ${labelStyle}`}>
+        <p>{label}</p>
         <input
-          type="number"
-          name=""
-          className={`gap rounded-md px-4 py-2 bg-stone-700 ring-stone-600 active:ring-stone-800 `}
+          type={type}
+          className={`gap rounded-md px-4 py-2 bg-stone-700 ring-stone-600 active:ring-stone-800 ${inputStyle}`}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
         />
       </label>
     </div>
