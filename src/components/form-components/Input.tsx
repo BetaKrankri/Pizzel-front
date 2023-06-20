b357f897aa2945739135e556b9c1b85d0b80a2d2
BetaKrankri <edominguezj11@outlook.com>
2023-06-19 19:06:40 -0600

    chore:Error msgs, Input+

--- src/components/form-components/Input.tsx
+++ src/components/form-components/Input.tsx
@@ -1,22 +1,24 @@
 import React, { InputHTMLAttributes } from "react";
 
 interface Props {
-  type: "text" | "password" | "email";
   label: string;
+  type: "text" | "password" | "email";
+  value?: string | number | readonly string[];
   placeholder?: string;
   labelStyle?: string;
   inputStyle?: string;
-  value?: string | number | readonly string[] | undefined;
+  autocomplete?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
 }
 
 const Input = ({
+  label,
   type,
+  value,
+  placeholder,
   labelStyle,
   inputStyle,
-  label,
-  placeholder,
-  value,
+  autocomplete,
   onChange,
 }: Props) => {
   return (
@@ -25,9 +27,10 @@ const Input = ({
         <p>{label}</p>
         <input
           type={type}
-          className={`gap rounded-md px-4 py-2 bg-stone-700 ring-stone-600 active:ring-stone-800 ${inputStyle}`}
-          placeholder={placeholder}
           value={value}
+          placeholder={placeholder}
+          className={`gap rounded-md px-4 py-2 bg-stone-700 ring-stone-600 active:ring-stone-800 ${inputStyle}`}
+          autoComplete={autocomplete}
           onChange={onChange}
         />
       </label>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
 import logo from "../assets/pizzel-logo.png";
 import Input from "../components/form-components/Input.tsx";
+import { useState } from "react";
+import axios from "axios";
 
 const RegisterPage = () => {
   const [form, setForm] = useState({
@@ -10,23 +11,36 @@ const RegisterPage = () => {
     confirmedPass: "",
   });
 
+  const [error, setError] = useState("");
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const sendRegisterData = async () => {
+      return;
+    };
+
+    if (!Object.values(form).every((inpVal) => inpVal)) {
+      setError(() => "All fields are required");
+    }
+
+    console.log("Sended");
   };
 
   return (
     <div className="RegisterPage w-full h-screen flex justify-center ">
-      <div className=" w-4/12 flex flex-col items-center py-12 gap-6">
+      <div className="Wrapper w-4/12 flex flex-col items-center py-12 gap-6 relative">
         <img src={logo} alt="logo" className="w-20" />
         <p className="text-xl">Log in to Pizzel</p>
         <form
           onSubmit={handleSubmit}
-          className=" w-full flex flex-col p-6 gap-4 rounded bg-stone-900"
+          className=" w-full flex flex-col p-6 gap-4 rounded bg-stone-900 "
         >
           <Input
             type="text"
             label="Display Name"
             placeholder="display name"
+            autocomplete="username"
             value={form.displayName}
             onChange={(e) => {
               setForm((f) => ({ ...f, displayName: e.target.value }));
@@ -41,6 +55,7 @@ const RegisterPage = () => {
               setForm((f) => ({ ...f, email: e.target.value }));
             }}
           />
+          {/* TODO: El password debe ser mayor a 8 chars */}
           <Input
             type="password"
             label="Password"
@@ -63,6 +78,17 @@ const RegisterPage = () => {
             Sign Up
           </button>
         </form>
+
+        <div
+          className={`bg-red-600 ring-red-500  text-white text-sm whitespace-nowrap rounded ${
+            error ? "w-full p-2 ring" : "w-0 p-0"
+          } overflow-hidden transition-all relative -top-3 flex items-center`}
+        >
+          <p className="w-full">{error}</p>
+          <button className="" onClick={() => setError("")}>
+            x
+          </button>
+        </div>
       </div>
     </div>
   );
