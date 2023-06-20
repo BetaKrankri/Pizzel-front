84dc0fcc178e1e7ca27579d5fc45327650bc9a00
BetaKrankri <edominguezj11@outlook.com>
2023-06-19 22:00:35 -0600

    chore: added Router to login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import EditorPage from "./pages/EditorPage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-      {/* <RegisterPage /> */}
-      {/* <Editor /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<EditorPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/EditorPage.tsx
@@ -0,0 +1,7 @@
+interface EditorPageProps {}
+
+const EditorPage: React.FunctionComponent<EditorPageProps> = (props) => {
+  return <div className="w-full h-screen">This is thee Editor Page</div>;
+};
+
+export default EditorPage;
