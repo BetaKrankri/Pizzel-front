bef32ee413ad758c7baabfc2dabde3be4368129c
BetaKrankri <edominguezj11@outlook.com>
2023-06-21 17:09:50 -0600

    chore: add a testform form APi

--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -1,7 +1,25 @@
-interface EditorPageProps {}
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { TestAPI } from "../components/TestAPI";
 
-const EditorPage: React.FunctionComponent<EditorPageProps> = (props) => {
-  return <div className="w-full h-screen">This is thee Editor Page</div>;
+const EditorPage: React.FunctionComponent = () => {
+  const authCtx = useContext(AuthContext);
+
+  return (
+    <div className="w-full h-screen flex flex-col items-center gap-4">
+      This is the Editor Page
+      <button
+        className="bg-stone-800 p-4 ring ring-stone-400"
+        onClick={() => {
+          localStorage.removeItem("loggedUser");
+          authCtx?.setLoggedUser(() => undefined);
+        }}
+      >
+        Cerrar sesion
+      </button>
+      <TestAPI />
+    </div>
+  );
 };
 
 export default EditorPage;
