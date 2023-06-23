b232f878744c62a64ef336235f1f146e90979366
BetaKrankri <edominguezj11@outlook.com>
2023-06-23 11:40:22 -0600

    comonent: Portfolio Button

---
+++ src/components/form-components/PortfolioButton.tsx
@@ -0,0 +1,31 @@
+import verticalDots from "../../assets/verticalDots.png";
+import { Portfolio, Canvas } from "../../utils/api";
+
+const PortfolioButton: React.FC<{ portfolio: Portfolio | undefined }> = ({
+  portfolio,
+}) => {
+  return (
+    <button
+      className={`PortfolioButton flex items-center justify-between text-start py-2 px-3
+         bg-slate-800 hover:bg-slate-700 rounded border-b border-b-slate-700 shadow-lg`}
+    >
+      <div className="flex flex-col w-full gap-1">
+        <p className="truncate text-lg font-jost ">
+          {portfolio?.name || "Portfolio name"}
+        </p>
+        <p className="text-sm font-jost">
+          {getCanvasString(portfolio?.canvases)}
+        </p>
+      </div>
+
+      <div className="w-4 h-4">
+        <img src={verticalDots} alt="submenu button" />
+      </div>
+    </button>
+  );
+};
+
+const getCanvasString = (canvases: Canvas[] | undefined) =>
+  `${canvases?.length || 0} canvas${canvases?.length === 1 ? "" : "es"}`;
+
+export default PortfolioButton;
