1c309a23195d4c6ecb8a00736bb556e24d1c8b07
BetaKrankri <edominguezj11@outlook.com>
2023-06-23 11:41:08 -0600

    componnent: TabButton

--- src/components/form-components/TabButton.tsx
+++ src/components/form-components/TabButton.tsx
@@ -4,10 +4,12 @@ const TabButton: React.FC<{
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
   isActive?: boolean;
 }> = ({ label, onClick, isActive }) => {
+  const activeStyle = isActive ? { backgroundColor: "#1E293B" } : {};
   return (
     <button
-      className={`TabButton px-5 py-3 text-lg text-start rounded-sm min-w-[180px] transition-all
-       hover:bg-slate-900 ${isActive && "bg-slate-800 hover:bg-slate-800"}`}
+      className={`TabButton px-5 py-3 rounded-sm min-w-[180px] text-start transition-all
+      bg-slate-950 hover:bg-slate-900`}
+      style={activeStyle}
       onClick={onClick}
     >
       {label}
