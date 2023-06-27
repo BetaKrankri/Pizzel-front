783c05f33cf39555302ba9ccf58a9ef7a01381ab
BetaKrankri <edominguezj@gmail.com>
2023-06-26 22:08:50 -0600

    chore: add portfolios synching

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,9 @@
 import React, { createContext, useEffect, useState } from "react";
-import { User } from "../utils/api";
+import { User, getAllPortfolios } from "../utils/api";
 
 export interface IAuthContext {
   loggedUser: User;
-  setLoggedUser: React.Dispatch<React.SetStateAction<User>>;
+  updateLoggedUser: React.Dispatch<React.SetStateAction<User>>;
 }
 
 export const AuthContext = createContext<IAuthContext | undefined>(undefined);
@@ -11,26 +11,43 @@ export const AuthContext = createContext<IAuthContext | undefined>(undefined);
 export const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [loggedUser, setLoggedUser] = useState<User>({ token: "" });
+  const [loggedUser, updateLoggedUser] = useState<User>({ token: "" });
 
+  // Al cargar la pagina, si el contexto no tiene User.token
+  // busca por un usuario almacenado en el localStorage
   useEffect(() => {
-    if (!loggedUser.token) {
+    const searchUserInfo = async () => {
       const storedInfo = localStorage.getItem("loggedUser");
-
       if (storedInfo) {
         const storedUser: User = JSON.parse(storedInfo);
-        storedUser.token && setLoggedUser(() => storedUser);
+        if (storedUser.token) {
+          const synchedPortfolios = await getAllPortfolios(storedUser);
+          updateLoggedUser(() => ({
+            ...storedUser,
+            portfolios: synchedPortfolios,
+          }));
+        }
       }
+    };
+
+    if (!loggedUser.token) {
+      searchUserInfo();
     }
+
+    // TODO: SYNCRONIZAR USUARIO Y SUS PORTAFOLIOS
   }, []);
 
+  // Actualiza el localStorage al actualizarse el 'loggedUser'
   useEffect(() => {
     console.log("AuthContext > loggedUser : updates  =>", loggedUser);
     if (!loggedUser?.token) return;
     localStorage.setItem("loggedUser", JSON.stringify(loggedUser));
   }, [loggedUser]);
 
-  const AuthCtxVal: IAuthContext = { loggedUser, setLoggedUser };
+  const AuthCtxVal: IAuthContext = {
+    loggedUser,
+    updateLoggedUser,
+  };
 
   return (
     <AuthContext.Provider value={AuthCtxVal}>{children}</AuthContext.Provider>
